e802325e3b2b0342d91dedec469825518b91852e
unic-greg <gregor.borosa@gmail.com>
2015-12-25 19:53:56 +0100

    added some debug logging

--- src/main.cpp
+++ src/main.cpp
@@ -2025,9 +2025,9 @@ bool LoadBlockIndex(bool fAllowNew)
         }
 
         //// debug print
-        printf("%s\n", block.GetHash().ToString().c_str());
-        printf("%s\n", hashGenesisBlock.ToString().c_str());
-        printf("%s\n", block.hashMerkleRoot.ToString().c_str());
+        printf("block.GetHash():%s\n", block.GetHash().ToString().c_str());
+        printf("hashGenesisBlock:%s\n", hashGenesisBlock.ToString().c_str());
+        printf("block.hashMerkleRoot:%s\n", block.hashMerkleRoot.ToString().c_str());
         assert(block.hashMerkleRoot == uint256("0x2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700"));
 
         // If genesis block hash does not match, then generate new genesis hash.
