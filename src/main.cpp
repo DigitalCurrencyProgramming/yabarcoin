e21b98dbe51225c14eea659686bf35da0c25e593
unic-greg <gregor.borosa@gmail.com>
2015-12-25 19:26:28 +0100

    setting new coin parameters

--- src/main.cpp
+++ src/main.cpp
@@ -2002,7 +2002,7 @@ bool LoadBlockIndex(bool fAllowNew)
     
         
         // Genesis block
-        const char* pszTimestamp = "Building a coin to show the world how...";
+        const char* pszTimestamp = "Building a yabarcoin for a little more entropy ...";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2014,14 +2014,14 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1369623856; //epochtime
+        block.nTime    = 1451067356; //epochtime, birthday on christmas 25.12.2015, ha!
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 1345972;
+        block.nNonce   = 0; //org:1345972;
 
         if (fTestNet)
         {
-            block.nTime    = 1369591342;
-            block.nNonce   = 440824;
+            block.nTime    = 1451067356;
+            block.nNonce   = 0; //org:440824;
         }
 
         //// debug print
